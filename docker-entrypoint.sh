3e84b40e97e51d700210f06000c10307397434ef
fjcloud <fj@msl.cloud>
2019-08-23 17:18:00 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,14 +11,14 @@ tail -f $ROOTPATH/logs/seahub.log &
 tail -f $ROOTPATH/logs/seafile.log &
 tail -f $ROOTPATH/logs/controller.log &
 
-maxretry=4
+maxretry=100
 retry=0
 
 while [ "$retry" -le "$maxretry" ]; do
     ps aux | grep seafile-controller | grep -v grep > /dev/null 2> /dev/null || {
         retry=$(expr $retry + 1);
     }
-    sleep 1000
+    sleep 5
 done
 echo "Seafile not running"
 exit 1
